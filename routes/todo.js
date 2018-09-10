ecc93bff6e245a692b4f0e8fd68a5b4c616223a5
சாமி. ஜெகன் லங்கா [Jagan Langa S] <jlanga@pivot3.com>
2018-09-10 16:58:28 +0530

    updated todo route to reuse the code for handling db

--- routes/todo.js
+++ routes/todo.js
@@ -35,45 +35,11 @@ const TODO_STATUS = {
     CANCELLED: 4
 }
 
-let db = new sqlite3.Database('./data/jagan-todos.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (error) => {
-    if (error) {
-        // TODO ::
-        console.error('Error Opening DB', error)
-    } else {
-        console.info('Connected to DB');
-
-        db.serialize(() => {
-
-            // sql to get the list of table names in the sqlite db.
-            // SELECT `name`, `sql` FROM `sqlite_master` WHERE type='table';
-
-            db.run('CREATE TABLE AI(id, label, status, ts)', [], (error) => {
-                if (error) {
-                    console.error('Failed to create tabel AI', error);
-                } else {
-                    insertDefaultValues();
-                    isConnectedToDB = true;
-                }
-            });
-        });
-    }
-});
-
-function insertDefaultValues() {
-    db.parallelize(() => {
-        let todos = tempTODOs.map((todo, index) => ({ label: todo, id: index, status: TODO_STATUS.OPEN }));
-        todos.forEach(todo => {
-            let sql = `INSERT INTO AI(id, label, status, ts) VALUES(?, ?, ?, ?)`;
-            db.run(sql, [todo.id, todo.label, todo.status, Date.now()], (error) => {
-                if (error) {
-                    console.error(error);
-                }
-            });
-        });
-    });
+const DB_RUN_MODE = {
+    DEFAULT: false,
+    PARALLIZE: 'parallize',
+    SERIALIZE: 'serialize',
 }
-
-db.close();
 
 class ResponceVo {
     constructor(status, data, message = null, code = 0) {
@@ -84,70 +50,99 @@ class ResponceVo {
     }
 }
 
+const requestHandler = (error, result, req, res, next) => {
+    if (error) {
+        res.send(new ResponceVo(false, error, error.message));
+    } else {
+        res.send(new ResponceVo(true, result));
+    }
+};
+
+function run(sql, params = [], callback = null, runMode = false, useAll=false) {
+    let db = new sqlite3.Database('./data/jagan-todos.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (error) => {
+        if (error) {
+            callback && callback(error);
+        } else {
+            if (runMode) {
+                db[runMode](() => {
+                    // TODO :: modify this case to run multiple sql's
+                    // TODO :: conside useAll
+                    // TODO :: how to make a single 'callback' when running multiple [COMMANDS] ?
+                    db.run(sql, params, (error, rows) => {
+                        if (error) {
+                            callback && callback(error);
+                        } else {
+                            callback && callback(null, rows);
+                        }
+                    });
+                    db.close();
+                });
+            } else {
+                let command = useAll ? 'all' : 'run';
+
+                db[command](sql, params, (error, result) => {
+                    if (error) {
+                        callback && callback(error);
+                    } else {
+                        callback && callback(null, result);
+                    }
+                });
+                db.close();
+            }
+        }
+    });
+}
+
+function initilizeDB() {
+    run('CREATE TABLE AI(id, label, status, ts)', [], (error, result) => {
+        if (error) {
+            console.error('Failed to create tabel AI', error);
+        } else {
+            insertDefaultValues();
+            isConnectedToDB = true;
+        }
+    }, DB_RUN_MODE.DEFAULT);
+}
+
+
+function insertDefaultValues() {
+    let todos = tempTODOs.map((todo, index) => ({ label: todo, id: index, status: TODO_STATUS.OPEN }));
+    let query = [], params = [];
+    todos.forEach(todo => {
+        query.push('(?, ?, ?, ?)');
+        params.push(todo.id, todo.label, todo.status, Date.now());
+    });
+    let sql = `INSERT INTO AI(id, label, status, ts) VALUES${query.join(', ')}`;
+    run(sql, params, (error, result) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.info('Initilized AI table with default values');
+        }
+    });
+}
+
+initilizeDB();
 
 /* ALL todo listing. */
 router.all('/', function (req, res, next) {
-
     let sql = `SELECT * FROM AI ORDER BY id`;
-
-    let db = new sqlite3.Database('./data/jagan-todos.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (error) => {
-        if (error) {
-            console.error('Error Opening DB', error);
-        } else {
-            db.all(sql, [], (error, rows) => {
-                if (error) {
-                    res.send(new ResponceVo(false, error, 'DB Error'));
-                } else {
-                    res.send(new ResponceVo(true, rows));
-                }
-                db.close();
-            });
-        }
-    });
+    run(sql, [], (error, result) => requestHandler(error, result, req, res, next), DB_RUN_MODE.DEFAULT, true);
 });
-
 
 router.all('/update', function (req, res, next) {
 
-    let db = new sqlite3.Database('./data/jagan-todos.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (error) => {
-        if (error) {
-            console.error('Error Opening DB', error);
-        } else {
-
-            db.serialize(() => {
-                let payload = req.body;
-                let sql = `UPDATE AI SET status=?, label=? WHERE id=?`;
-                db.run(sql, [payload.status, payload.label, payload.id], (error, rows) => {
-                    if (error) {
-                        res.send(new ResponceVo(false, error, error.message));
-                    } else {
-                        res.send(new ResponceVo(true, rows));
-                    }
-                    db.close();
-                });
-            });
-        }
-    });
+    let payload = req.body;
+    let sql = `UPDATE AI SET status=?, label=? WHERE id=?`;
+    run(sql, [payload.status, payload.label, payload.id], (error, result) => requestHandler(error, result, req, res, next), DB_RUN_MODE.SERIALIZE);
 });
 
 router.all('/add', function (req, res, next) {
 
-    let db = new sqlite3.Database('./data/jagan-todos.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (error) => {
-        if (error) {
-            console.error('Error Opening DB', error);
-        } else {
-            let todo = req.body;
-            let sql = `INSERT INTO AI(id, label, status, ts) VALUES(?, ?, ?, ?)`;
-            
-            db.run(sql, [Date.now(), todo.label, todo.status, Date.now()], (error) => {
-                if (error) {
-                    res.send(new ResponceVo(false, error, error.message));
-                } else {
-                    res.send(new ResponceVo(true, null));
-                }
-            });
-        }
-    });
+    let todo = req.body;
+    let sql = `INSERT INTO AI(id, label, status, ts) VALUES(?, ?, ?, ?)`;
+
+    run(sql, [Date.now(), todo.label, todo.status, Date.now()], (error, result) => requestHandler(error, result, req, res, next));
 });
 
 module.exports = router;
